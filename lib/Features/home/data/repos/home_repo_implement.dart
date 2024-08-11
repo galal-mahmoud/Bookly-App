949a17dc1da59ad7fb3c7ffd0ee7214307d8b011
galal-mahmoud <galalengineer45@gmail.com>
2024-08-11 15:25:42 +0200

    make implementations for home repo implement

--- lib/Features/home/data/repos/home_repo_implement.dart
+++ lib/Features/home/data/repos/home_repo_implement.dart
@@ -1,15 +1,45 @@
 import 'package:bookly_app_v1/Features/home/data/repos/home_repo.dart';
-import 'package:either_dart/either.dart';
+import 'package:bookly_app_v1/core/utils/api_services.dart';
+import 'package:dio/dio.dart';
 import '../../../../core/errors/failure.dart';
 import '../models/book_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepoImplement implements HomeRepo{
+  ApiServices apiServices;
+  HomeRepoImplement(this.apiServices);
   @override
-  Future<Either<Failure, List<BookModel>>> fetcthBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async{
+    try {
+      var data = await apiServices.get(endPoint: 'volumes?q=subject:programming');
+      List<BookModel> books = [];
+      for(var item in data['items']){
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if(e is DioException){
+        return left(ServerFailure.fromDioException(e));
+      }else{
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
   }
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    throw UnimplementedError();
+  Future<dynamic> fetcthBestSellerBooks() async{
+    try {
+      var data = await apiServices.get(endPoint: 'volumes?q=subject:programming');
+      List<BookModel> books = [];
+      for(var item in data['items']){
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if(e is DioException){
+        return left(ServerFailure.fromDioException(e));
+      }else{
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,46 @@
-abstract class Failure{}
+
+import 'package:dio/dio.dart';
+
+abstract class Failure{
+  final String errorMessage;
+  Failure({required this.errorMessage});
+}
+
+class ServerFailure extends Failure{
+  ServerFailure({required super.errorMessage});
+  factory ServerFailure.fromDioException(DioException dioException){
+    switch(dioException.type){
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errorMessage: 'Connection timeout, please try later');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errorMessage: 'Send timeout, please try later');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(errorMessage: 'Receive timeout, please try later');
+      case DioExceptionType.badCertificate:
+        return ServerFailure(errorMessage: 'Bad certificate, please try later');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure(errorMessage: 'Request to ApiServer was canceled, please try again');
+      case DioExceptionType.connectionError:
+        return ServerFailure(errorMessage: 'There is a problem with internet connection, please try again');
+      case DioExceptionType.unknown:
+        if(dioException.message!.contains('SocketException')){
+          return ServerFailure(errorMessage: 'Internet connection drop, please try again');
+        }else{
+          return ServerFailure(errorMessage: 'UnExpected error, please try again');
+        }
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response){
+    if(statusCode == 400 || statusCode == 401 || statusCode == 403){
+      return ServerFailure(errorMessage: response['error']['message']);
+    }else if(statusCode == 404){
+      return ServerFailure(errorMessage: 'Your request not found, please try later');
+    }else if(statusCode == 500){
+      return ServerFailure(errorMessage: 'There is a problem with internet sever, please try later');
+    }else{
+      return ServerFailure(errorMessage: 'Oops there was an error, please try later');
+    }
+  }
+}
